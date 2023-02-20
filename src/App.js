cf5b5674dab225e5e44e05c46b2510cc23fcd282
beka.dev <bigsamsa3@mail.ru>
2023-02-21 00:32:40 +0600

    add new components Forecast.jsx

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {TopButtons} from "./components/TopButtons";
 import {Inputs} from "./components/Inputs";
 import {TimeAndLocation} from "./components/TimeAndLocation";
 import {TemperatureAndDetails} from "./components/TemperatureAndDetails";
+import {Forecast} from "./components/Forecast";
 
 export const App = () => {
 
@@ -14,6 +15,8 @@ export const App = () => {
             <Inputs />
             <TimeAndLocation />
             <TemperatureAndDetails />
+            <Forecast title={'hourly forecast'}/>
+            <Forecast title={'daily forecast'}/>
         </div>
     </div>
   );
